6496d56b4264cdeb045d0f0d4144a87b9a780c0d
Erik <enilson09@gmail.com>
2022-07-15 15:33:59 -0700

    Added more unit tests

--- test.py
+++ test.py
@@ -16,12 +16,36 @@ class FlaskTestCase(unittest.TestCase):
         self.assertTrue(b'Please login' in response.data)
 
     # Ensure login behaves correctly given correct credentials
+    def test_correct_login(self):
+        tester = app.test_client(self) # used to create a test mocking up the functionality of current app
+        response = tester.post('/login', data=dict(username="admin", password="admin"), follow_redirects=True)
+        self.assertIn(b'You were just logged in!', response.data)
 
 
     # Ensure login behaves correctly given incorrect credentials
-
+    def test_incorrect_login(self):
+        tester = app.test_client(self) # used to create a test mocking up the functionality of current app
+        response = tester.post('/login', data=dict(username="wrong", password="wrong"), follow_redirects=True)
+        self.assertIn(b'Invalid credentials. Please try again.', response.data)
 
     # Ensure logout behaves correctly
+    def test_logout(self):
+        tester = app.test_client(self) # used to create a test mocking up the functionality of current app
+        tester.post('/login', data=dict(username="admin", password="admin"), follow_redirects=True)
+        response = tester.get('/logout', follow_redirects=True)
+        self.assertIn(b'You were just logged out!', response.data)
+
+    # Ensure main page requires login
+    def test_main_route_requires_login(self):
+        tester = app.test_client(self) # used to create a test mocking up the functionality of current app
+        response = tester.get('/', follow_redirects=True)
+        self.assertTrue(b'You need to login first' in response.data)
+
+    # Ensure that posts show up on the main page
+    def test_post_show_up(self):
+        tester = app.test_client(self) # used to create a test mocking up the functionality of current app
+        response = tester.post('/login', data=dict(username="admin", password="admin"), follow_redirects=True)
+        self.assertIn(b'Good', response.data)
 
 if __name__ == '__main__':
     unittest.main()
